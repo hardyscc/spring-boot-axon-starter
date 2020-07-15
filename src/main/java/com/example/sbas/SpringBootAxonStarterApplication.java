fdafd8181e8c253d7466d5cf3106a0a2ec265f65
Hardys <hardyscc@gmail.com>
2020-07-15 16:21:33 +0800

    initial commit

---
+++ src/main/java/com/example/sbas/SpringBootAxonStarterApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAxonStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAxonStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sbas/SpringBootAxonStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAxonStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
