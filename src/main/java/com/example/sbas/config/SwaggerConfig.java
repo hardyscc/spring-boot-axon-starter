5c456e6bc3c6a211fb9e1d342e966aef6d79065f
Hardys <hardyscc@gmail.com>
2020-07-17 12:10:27 +0800

    chore: simple swagger config

---
+++ src/main/java/com/example/sbas/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.example.sbas.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI(BuildProperties buildProperties) {
+        return new OpenAPI().info(new Info()
+                .title("Bank Accounts API")
+                .version(buildProperties.getVersion()));
+    }
+}
